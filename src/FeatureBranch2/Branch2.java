03120cbc7279aed106b6cf7868f2cbbe86cdf6a9
Murali <muralimsdian1997@gmail.com>
2025-09-13 22:57:56 +0530

    Revert "Feature  Branch2 commit"
    This reverts commit f92cd028

--- src/FeatureBranch2/Branch2.java
+++
@@ -1,9 +0,0 @@
-package FeatureBranch2;
-
-public class Branch2 {
-    public static void main(String[] args) {
-        System.out.println("Feature Branch 2");
-        System.out.println("Git stash apply");
-        System.out.println("Git stash apply2");
-    }
-}
